a0d271cb7a61ead70e4a304f7d7272f7e9499cdd
devdigital <devdigital.cloud@gmail.com>
2019-12-10 14:28:55 +0000

    wip: update lexer story

--- packages/parse/src/lexer.stories.js
+++ packages/parse/src/lexer.stories.js
@@ -2,23 +2,16 @@ import React from 'react'
 import { State, Store } from '@sambego/storybook-state'
 import lexer from './lexer'
 
-const Lexer = ({ value, onChange }) => {
-  let result = null
-  try {
-    console.log('value', value)
-    result = value ? lexer(value, 0) : null
-  } catch (error) {
-    result = error.message
-  }
-
+const Lexer = ({ text, results, error, onChange }) => {
   return (
     <>
       <input
         type="text"
-        value={value}
+        value={text}
         onChange={e => onChange(e.target.value)}
       />
-      <p>{JSON.stringify(result)}</p>
+      {results && results.length > 0 && <p>{JSON.stringify(results)}</p>}
+      {error && <p>There was an error: {error.message}</p>}
     </>
   )
 }
@@ -26,14 +19,42 @@ const Lexer = ({ value, onChange }) => {
 export default { title: 'Lexer' }
 
 const store = new Store({
-  value: '',
+  text: '',
+  results: [],
+  error: null,
 })
+
+const getLexerResults = text => {
+  const results = []
+
+  let position = 0
+  while (position < text.length) {
+    const result = lexer(text, position)
+    results.push(result)
+    position = result.position.end + 1
+  }
+
+  return results
+}
 
 export const withDefault = () => (
   <State store={store}>
     <Lexer
-      value={store.get('value')}
-      onChange={text => store.set({ value: text })}
+      text={store.get('text')}
+      onChange={text => {
+        if (!text) {
+          store.set({ text: '', results: [] })
+          return
+        }
+
+        try {
+          const results = getLexerResults(text)
+          store.set({ text, results, error: null })
+        } catch (error) {
+          store.set({ text, results: [], error })
+        }
+      }}
+      error={store.get('error')}
     />
   </State>
 )
