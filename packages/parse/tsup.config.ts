89228dd2f9707328a8a056740ad4895bada7c4b4
devdigital <devdigital.cloud@gmail.com>
2023-11-18 20:27:14 +0000

    wip: update branch

--- packages/parse/tsup.config.ts
+++ packages/parse/tsup.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -7,5 +7,4 @@ export default defineConfig({
   clean: true,
   dts: true,
   format: ['cjs', 'esm'],
-  external: ['react'],
-})
+});
